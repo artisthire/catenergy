a045d24236be7d221d01ecb17aa8d70419f9a8df
Andrey <artist_hire@ukr.net>
2018-12-10 14:35:56 +0200

    изменил поведение карты на главной странце в зависимости от вьюпорта

--- source/js/script.js
+++ source/js/script.js
@@ -27,18 +27,18 @@ toggle_btn.addEventListener('click', function() {
   }
 });
 
+// инициализация и добавление интерактивной карты на странциу
+
 var map_container = document.getElementById('main_contacts_map');
 var map_backup = document.getElementById('main_contacts_map_backup');
+
 //получаем ширину страницы браузера для изменения карты
 var screen_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
-var map = L.map(map_container, {scrollWheelZoom: false});
-var map_pin_width = 56;
-var map_pin_height = 53;
-var lat = 59.938727;
-var lng = 30.323085;
-
-
+// переменная, которая содержит слой карты с меткой. Необходимо для удаления/добавления маркера на разных вьюпортах
+var layer = null;
+// переменная содержит ссылку на добавленую карту
+var map = null;
 // инициализация карты
 initMap();
 
@@ -46,21 +46,9 @@ initMap();
 window.addEventListener('resize', function() {
   screen_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
-  if (screen_width >= 768) {
-    map_pin_width = 113;
-    map_pin_height = 106;
-  }
-
-  if (screen_width >= 1440) {
-    //на версии для десктопа центр карты смещаем вправо
-    map.setView([59.939012, 30.317150], 17);
-  }
-  else if (screen_width >= 768) {
-    map.setView([lat, lng], 18);
-  }
-  else {
-    map.setView([lat, lng], 17);
-  }
+  // удаляем маркер с карты для его реинициализации на новых вьюпорта (чтобы небыло наложения)
+  layer.removeFrom(map);
+  initMap();
 });
 
 function initMap() {
@@ -69,19 +57,33 @@ function initMap() {
   map_backup.style.display = 'none';
   map_container.style.display = "block";
 
-  // var lat = 59.938727;
-  // var lng = 30.323085;
+  // задаем координаты маркера
+  var lat = 59.938727;
+  var lng = 30.323085;
 
+  //задаем высоту и ширину маркера
+  var map_pin_width = 56;
+  var map_pin_height = 53;
+  // задаем относительную точку позиционирования маркера над координатами
+  var map_pin_anchor_g = Math.round(map_pin_width / 2);
+  var map_pin_anchor_v = map_pin_height - 1;
+
+  // на экранах планшета и больше иконка макера больше
   if (screen_width >= 768) {
     map_pin_width = 113;
     map_pin_height = 106;
   }
 
-  var map_pin_anchor_g = Math.round(map_pin_width / 2);
-  var map_pin_anchor_v = map_pin_height - 1;
+  //инициализация карты
+  //отключаем возможность увеличения карты по скролингу, чтобы не мешало пролистыванию страницы
+  if (map == null) {
+    map = L.map(map_container, {scrollWheelZoom: false});
+  }
 
+  //различные представления карты на разных вьюпортах
   if (screen_width >= 1440) {
     //на версии для десктопа центр карты смещаем вправо
+    //уменьшаем стартовый зум до 17
     map.setView([59.939012, 30.317150], 17);
   }
   else if (screen_width >= 768) {
@@ -91,17 +93,24 @@ function initMap() {
     map.setView([lat, lng], 17);
   }
 
+  //инициализация тайтлов
+  // для карты gogle параметр lyrs означает
+  // lyrs=m - городсткая карта
+  // lyrs=s - спутниковые снимки
+  // lyrs=s,h - гибрид
 	L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3'],
     attribution: '&copy; <a href="https://www.google.com/permissions/geoguidelines/">Google Maps</a> contributors'
   }).addTo(map);
 
+  //инициализация иконки с указанием адреса картинки
   var myIcon = L.icon({
     iconUrl: '../img/map-pin.png',
     iconSize: [map_pin_width, map_pin_height],
     iconAnchor: [map_pin_anchor_g, map_pin_anchor_v]
   });
 
-  L.marker([lat, lng], {icon: myIcon}).addTo(map);
+  //добавление макера на карту
+  layer = L.marker([lat, lng], {icon: myIcon}).addTo(map);
 }
